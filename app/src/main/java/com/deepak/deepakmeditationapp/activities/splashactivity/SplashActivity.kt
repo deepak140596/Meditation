0ed48598ce9a9069f4afbea644c019ad30abd19e
Deepak Prasad <deepak140596@gmail.com>
2019-07-29 11:21:59 +0530

    Added Splash Activity

--- app/src/main/java/com/deepak/deepakmeditationapp/activities/splashactivity/SplashActivity.kt
+++ app/src/main/java/com/deepak/deepakmeditationapp/activities/splashactivity/SplashActivity.kt
@@ -2,12 +2,23 @@ package com.deepak.deepakmeditationapp.activities.splashactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.deepak.deepakmeditationapp.R
+import android.content.Intent
+import com.deepak.deepakmeditationapp.MainActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
+    val SPLASH_LENGTH = 3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_LENGTH)
     }
 }
