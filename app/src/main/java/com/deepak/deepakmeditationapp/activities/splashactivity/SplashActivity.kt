e7132bb0b7d608cdeff58e9aa4d1a40f91181a88
Deepak Prasad <deepak140596@gmail.com>
2019-07-29 11:10:19 +0530

    Configured firebase

---
+++ app/src/main/java/com/deepak/deepakmeditationapp/activities/splashactivity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.deepak.deepakmeditationapp.activities.splashactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.deepak.deepakmeditationapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
